53938a726dc67609f9ed218eb6f9821468eaf17a
spurthipethakamsetti <spurthipethakamsetti@gmail.com>
2026-05-04 17:52:02 +0530

    Update speech_module.py

--- speech_module.py
+++ speech_module.py
@@ -1,10 +1,7 @@
 from gtts import gTTS
-import os
 
 def text_to_speech(text):
     tts = gTTS(text=text, lang='en')
     file_path = "output.mp3"
     tts.save(file_path)
-
-    # Open with default system player
-    os.system(f"start {file_path}")
+    return file_path   # return file instead of playing
